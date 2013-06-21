d01502e7237538407c97b528fb23e554167a484f
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-06-21 13:54:51 -0400

    added optional command-line argument for output file name; new code comments; renamed some methods

--- dexcom_to_JSON.py
+++ dexcom_to_JSON.py
@@ -6,26 +6,29 @@ import util_time
 class StudioReader():
     """Reads Dexcom Studio .csv export file and encodes to JSON."""
 
-    def __init__(self, dex):
+    def __init__(self, dex, output = ""):
 
         self.dexcom = {'Calibrations': [], 'Readings': []}
 
-        if dex[1]:
+        self.output_filename = output
+
+        # store UTC offset if given on command line with Dexcom file
+        try:
             self.offset = dex[1]
-        else:
+        except IndexError as i1:
             self.offset = None
 
         with open(dex[0], 'rb') as f:
             self.rdr = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
-            self.get_readings()
+            self._get_readings()
 
         with open(dex[0], 'rb') as f:
             self.rdr = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
-            self.get_calibs()
+            self._get_calibs()
 
         self.create_JSON()
 
-    def get_readings(self):
+    def _get_readings(self):
         """Read blood glucose values and timestamps from Dexcom export file and save in a dict."""
 
         readings = self.dexcom['Readings']
@@ -44,7 +47,7 @@ class StudioReader():
                 "UTC_timestamp": util_time.dexcom_to_ISO8601(row[2], self.offset),
                 "blood_glucose": numeric})
 
-    def get_calibs(self):
+    def _get_calibs(self):
         """Read blood glucose calibrations and timestamps from Dexcom export file and save in a dict."""
 
         calibs = self.dexcom['Calibrations']
@@ -67,17 +70,26 @@ class StudioReader():
     def create_JSON(self):
         """Transfer BG objects to a JSON file."""
 
-        with open("dexcom.json", 'w') as f:
+        if self.output_filename:
+            filename = self.output_filename
+        else:
+            filename = "dexcom.json"
+
+        with open(filename, 'w') as f:
             print >> f, json.dumps(self.dexcom, sort_keys=True, indent=4, separators=(',', ': '))
 
 def main():
     parser = argparse.ArgumentParser(description='Process the input Dexcom Studio file.')
-    parser.add_argument('-d', '--dexcom', nargs='+', action = 'store', dest = "dexcom", help='name of Dexcom Studio \
+    parser.add_argument('dexcom_file', nargs='+', action='store', help='name of Dexcom Studio \
         .csv file and optional Dexcom internal timezone offset as {+-}H')
+    parser.add_argument('-o', '--output', action='store', dest='output', help="name of output file; default is 'dexcom.json'")
 
     args = parser.parse_args()
 
-    d = StudioReader(args.dexcom)
+    if args.output:
+        d = StudioReader(args.dexcom_file, args.output)
+    else:
+        d = StudioReader(args.dexcom_file)
 
 if __name__ == '__main__':
     main()
