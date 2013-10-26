6b3c68e2273a0574af15d5ebb5e5ba3f9735a56c
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-10-26 13:09:47 -0700

    added small multiple width, height, margin to summary model

--- js/models/summary.js
+++ js/models/summary.js
@@ -1,6 +1,6 @@
 define(['underscore', 'backbone'],
 	function(_, Backbone) {
-		var FocusedSVG = Backbone.Model.extend({
+		var Summary = Backbone.Model.extend({
 			defaults: {
 				width: 860,
 				height: 530,
@@ -9,9 +9,12 @@ define(['underscore', 'backbone'],
 				margin_top: 20,
 				margin_right: 20,
 				margin_bottom: 20,
-				margin_left: 80
+				margin_left: 80,
+				sm_width: 200,
+				sm_height: 200,
+				sm_margin: 10
 			}
 		});
 
-	return FocusedSVG;
+	return Summary;
 });
